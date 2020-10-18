41fe325e01af537154ec1de9110860d323427fc8
seokho96 <superpower1226@gmail.com>
2020-10-18 16:59:09 +0900

    10/18 mainView

--- paperHouse/src/main/java/com/bit/paperhouse/dao/UserRepository.java
+++ paperHouse/src/main/java/com/bit/paperhouse/dao/UserRepository.java
@@ -1,8 +1,11 @@
 package com.bit.paperhouse.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
+import com.bit.paperhouse.dto.ArticleDto;
 import com.bit.paperhouse.dto.UserDto;
 import com.bit.paperhouse.model.CustomSecurityDetails;
 
@@ -13,11 +16,14 @@ public interface UserRepository {
 	
     public void regi( UserDto dto );
 	
-	
 	public CustomSecurityDetails findByid( String username );
 	
 	public UserDto findByEmail( String email );
 	
 	public void resetPassword(UserDto dto);
+	
+	public void emailCheckAf( UserDto dto );
+	
+	public List<ArticleDto> getArticleList(String category);
 
 }
